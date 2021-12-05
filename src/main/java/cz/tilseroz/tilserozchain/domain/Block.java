ce53160078b12986a2d55e5bf4ec88dbf5c5a59f
David Tilšer <davidtilser@gmail.com>
2021-12-05 18:44:39 +0100

    Custom Blockchain in Java

---
+++ src/main/java/cz/tilseroz/tilserozchain/domain/Block.java
@@ -0,0 +1,70 @@
+package cz.tilseroz.tilserozchain.domain;
+
+import cz.tilseroz.tilserozchain.util.FingerprintUtil;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Block {
+
+    private String hash; //digital signature
+    private String previousHash; // previous block's hash
+    private String data; // data
+    public ArrayList<Transaction> transactions = new ArrayList<>(); //our data will be a simple message.
+    public String merkleRoot;
+    private long timeStamp;
+    private int nonce;
+
+    //Block Constructor.
+    public Block(String previousHash) {
+        this.previousHash = previousHash;
+        this.timeStamp = new Date().getTime();
+
+        this.hash = calculateHash(); //Making sure we do this after we set the other values.
+    }
+
+    //Calculate new hash based on blocks contents
+    public String calculateHash() {
+        String calculatedhash = FingerprintUtil.applySha256(
+                previousHash +
+                        Long.toString(timeStamp) +
+                        Integer.toString(nonce) +
+                        merkleRoot
+        );
+        return calculatedhash;
+    }
+
+    //Increases nonce value until hash target is reached.
+    public void mineBlock(int difficulty) {
+        merkleRoot = FingerprintUtil.getMerkleRoot(transactions);
+        String target = new String(new char[difficulty]).replace('\0', '0');; //Create a string with difficulty * "0"
+        while (!hash.substring(0, difficulty).equals(target)) {
+            nonce++;
+            hash = calculateHash();
+        }
+        System.out.println("Block Mined!!! : " + hash);
+    }
+
+    //Add transactions to this block
+    public boolean addTransaction(Transaction transaction) {
+        //process transaction and check if valid, unless block is genesis block then ignore.
+        if (transaction == null) return false;
+        if ((previousHash != "0")) {
+            if ((!transaction.processTransaction())) {
+                System.out.println("Transaction failed to process. Discarded.");
+                return false;
+            }
+        }
+        transactions.add(transaction);
+        System.out.println("Transaction Successfully added to Block");
+        return true;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public String getPreviousHash() {
+        return previousHash;
+    }
+}
---
+++ src/test/java/cz/tilseroz/tilserozchain/TilserozchainApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.tilseroz.tilserozchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TilserozchainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
